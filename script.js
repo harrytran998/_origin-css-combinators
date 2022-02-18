62e96bd008d346eeed5fe3973064620afdacc596
harrytran998 <nhattq.coding@gmail.com>
2022-02-18 07:28:12 +0700

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello!')
+function handleSubmit(event) {
+  const cake = document.querySelector('#cake');
+  const selectedCake = document.querySelector('#selectedCake');
+  const candy = document.querySelector('#candy');
+  const selectedCandy = document.querySelector('#selectedCandy');
+
+  if (selectedCake && cake) {
+    selectedCake.innerHTML = cake.value;
+  }
+
+  if (selectedCandy && candy) {
+    selectedCandy.innerHTML = candy.value;
+  }
+}
